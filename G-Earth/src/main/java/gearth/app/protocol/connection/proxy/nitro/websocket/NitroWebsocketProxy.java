f81c6e75746a7b422393cf709fc00aa667fe9c80
BouchamaMehdideHabbocity <mehdi00bouchama@gmail.com>
2025-08-24 17:33:07 -0700

    Fix Habbix.me
    Handle WebSocket Pings

--- G-Earth/src/main/java/gearth/app/protocol/connection/proxy/nitro/websocket/NitroWebsocketProxy.java
+++ G-Earth/src/main/java/gearth/app/protocol/connection/proxy/nitro/websocket/NitroWebsocketProxy.java
@@ -5,6 +5,8 @@ import com.github.monkeywie.proxyee.intercept.HttpProxyInterceptPipeline;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PingWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PongWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,24 +26,46 @@ public class NitroWebsocketProxy extends HttpProxyIntercept {
         this.callback.onHandshakeComplete();
     }
 
-    @Override
-    public void onWebsocketRequest(Channel clientChannel, Channel proxyChannel, WebSocketFrame webSocketFrame, HttpProxyInterceptPipeline pipeline) throws Exception {
-        final byte[] data = getBinaryData(webSocketFrame);
-        if (data != null) {
-            this.callback.onClientMessage(data);
-        }
 
-        webSocketFrame.release();
+    @Override
+    public void onWebsocketRequest(Channel clientChannel, Channel proxyChannel, WebSocketFrame frame,
+                                   HttpProxyInterceptPipeline pipeline) throws Exception {
+        try {
+            // Si le client ping, on pong
+            if (frame instanceof PingWebSocketFrame ping) {
+                clientChannel.writeAndFlush(new PongWebSocketFrame(ping.content().retain()));
+                LOG.debug("WS CLIENT Ping -> Pong sent");
+                return;
+            }
+
+            final byte[] data = getBinaryData(frame);
+            if (data != null) {
+                this.callback.onClientMessage(data);
+            }
+        } finally {
+            frame.release();
+        }
     }
 
-    @Override
-    public void onWebsocketResponse(Channel clientChannel, Channel proxyChannel, WebSocketFrame webSocketFrame, HttpProxyInterceptPipeline pipeline) throws Exception {
-        final byte[] data = getBinaryData(webSocketFrame);
-        if (data != null) {
-            this.callback.onServerMessage(data);
-        }
 
-        webSocketFrame.release();
+    @Override
+    public void onWebsocketResponse(Channel clientChannel, Channel proxyChannel, WebSocketFrame frame,
+                                    HttpProxyInterceptPipeline pipeline) throws Exception {
+        try {
+            // Si le SERVEUR ping, on pong
+            if (frame instanceof PingWebSocketFrame ping) {
+                proxyChannel.writeAndFlush(new PongWebSocketFrame(ping.content().retain()));
+                LOG.debug("WS SERVER Ping -> Pong sent");
+                return;
+            }
+            // sinon ça marche normal
+            final byte[] data = getBinaryData(frame);
+            if (data != null) {
+                this.callback.onServerMessage(data);
+            }
+        } finally {
+            frame.release();
+        }
     }
 
     @Override
@@ -49,23 +73,21 @@ public class NitroWebsocketProxy extends HttpProxyIntercept {
         this.callback.onClose();
     }
 
+
+
     private byte[] getBinaryData(WebSocketFrame frame) {
         if (frame instanceof BinaryWebSocketFrame binaryFrame) {
             final ByteBuf content = binaryFrame.content();
-            final byte[] binaryData = new byte[binaryFrame.content().readableBytes()];
-
+            final byte[] binaryData = new byte[content.readableBytes()];
             content.markReaderIndex();
-
             try {
                 content.readBytes(binaryData);
             } finally {
                 content.resetReaderIndex();
             }
-
             return binaryData;
         }
 
-        LOG.error("Unexpected nitro frame type: {}", frame.getClass());
         return null;
     }
 }
