99947649aa2385c27b1085dbfe28698740d65824
QDave <davidav08@gmail.com>
2026-02-28 11:13:40 +0100

    anubis-fix

--- G-Earth/src/main/java/gearth/app/protocol/connection/proxy/nitro/os/windows/NitroWindows.java
+++ G-Earth/src/main/java/gearth/app/protocol/connection/proxy/nitro/os/windows/NitroWindows.java
@@ -16,7 +16,7 @@ public class NitroWindows implements NitroOsFunctions {
     /**
      * Semicolon separated hosts to ignore for proxying.
      */
-    private static final String PROXY_IGNORE = "discord.com;discordapp.com;canary.discord.com;canary.discordapp.com;github.com;gateway.discord.gg;";
+    private static final String PROXY_IGNORE = "discord.com;discordapp.com;canary.discord.com;canary.discordapp.com;github.com;gateway.discord.gg;websocket.habbo.network;";
 
     /**
      * Checks if the certificate is trusted by the local machine.
